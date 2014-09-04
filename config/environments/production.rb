dc49465b2cec4087a9d398a57cce830426a54b9f
delius <pawel@fibrotrust.org>
2014-09-04 15:20:55 +0100

    :

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,7 +53,7 @@ Dbsite::Application.configure do
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
-
+  config.assets.digest = true
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = "http://assets.example.com"
 
