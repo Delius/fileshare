f1c34fd98988902a8fd6eadcada3fa4be01c487e
delius <pawel@fibrotrust.org>
2014-09-04 15:09:31 +0100

    :

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,9 @@ Dbsite::Application.configure do
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
   
-  config.assets.precompile = ['*.js', '*.css', '*.css.erb']
+  # config.assets.precompile = ['*.js', '*.css', '*.css.erb']
+
+  config.assets.precompile += %w( *.css *.js )
   
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
